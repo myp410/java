2e1867e464e41f46ca635920204b4057483edc3f
EC2 Default User <ec2-user@ip-172-31-26-150.ap-northeast-1.compute.internal>
2024-07-11 10:18:23 +0000

    自動型変換

--- chapter2.java
+++ chapter2.java
@@ -1,11 +1,5 @@
 public class chapter2 {
   public static void main(String[] args) {
-    String prof1 = "名前：朝香あゆみ¥n(あさかあゆみ) ";
-    String prof2 = """
-                   名前：湊雄介
-                            (みなとゆうすけ)
-                   """;
-    System.out.println(prof1);
-    System.out.println(prof2);
+    int i = 3.2;
   }
 }
