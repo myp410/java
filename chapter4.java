0811529a349ddac84692c7f6203e12ba67d2d2b1
EC2 Default User <ec2-user@ip-172-31-26-150.ap-northeast-1.compute.internal>
2024-07-14 06:12:41 +0000

    集計

--- chapter4.java
+++ chapter4.java
@@ -1,8 +1,17 @@
 public class chapter4 {
   public static void main(String[] args) {
-    int[] scores;
-    scores = new int[5];
-    scores[1] = 30;
-    System.out.println(scores[1]);
+    int[] scores = {20, 30, 40, 50, 80};
+    int sum = 0;
+    int count = 0;
+    for (int i = 0; i< scores.length; i++) {
+      sum += scores[i];
+      if (scores[i] >= 50) {
+        count++;
+      }
+    }
+    int avg = sum / scores.length;
+    System.out.println("合計点:" + sum);
+    System.out.println("平均点:" + avg);
+    System.out.println("50点以上の科目の数は:" + count);
   }
 }
