fac33bcdde55e44421bb2ec6799af435dda0b5d6
EC2 Default User <ec2-user@ip-172-31-26-150.ap-northeast-1.compute.internal>
2024-07-14 06:02:05 +0000

    配列の設定

--- chapter4.java
+++ chapter4.java
@@ -1,7 +1,8 @@
 public class chapter4 {
   public static void main(String[] args) {
-    int[] scores = new int[5];
-    int num = scores.length;
-    System.out.println("要素の数: " + num);
+    int[] scores;
+    scores = new int[5];
+    scores[1] = 30;
+    System.out.println(scores[1]);
   }
 }
