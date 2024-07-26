61ecb70c2121ce895ad069698d8d908a55f75543
EC2 Default User <ec2-user@ip-172-31-26-150.ap-northeast-1.compute.internal>
2024-07-26 01:44:23 +0000

    オブジェクト志向

---
+++ chapter8/Hero.java
@@ -0,0 +1,20 @@
+public class Hero {
+  String name;
+  int hp;
+  public void attack() {
+    
+  }
+  public void sleep() {
+    this.hp = 100;
+    System.out.println(this.name + "は、眠って回復した！");
+  }
+  public void sit(int sec) {
+    
+  }
+  public void slip() {
+    
+  }
+  public void run() {
+    
+  }
+}
