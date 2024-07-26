67eea615f55454decdba77f9ee60f2d987bab854
EC2 Default User <ec2-user@ip-172-31-26-150.ap-northeast-1.compute.internal>
2024-07-26 01:50:35 +0000

    メソッド追加

--- chapter8/Hero.java
+++ chapter8/Hero.java
@@ -9,12 +9,18 @@ public class Hero {
     System.out.println(this.name + "は、眠って回復した！");
   }
   public void sit(int sec) {
-    
+    this.hp += sec;
+    System.out.pritnln(this.name + "は、" + sec + "秒座った！");
+    System.out.pritnln("HPが" + sec + "ポイント回復した");
   }
   public void slip() {
-    
+    this.hp -= 5;
+    System.out.println(this.name + "は、転んだ！");
+    System.out.println("5のダメージ！");
   }
   public void run() {
-    
+    System.out.println(this.name + "は、逃げ出した！");
+    System.out.println("GAMEOVER");
+    System.out.println("最終HPは" + this.hp + "でした");
   }
 }
