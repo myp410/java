f872d66ed5609a2525093c220ac59266d4b3904f
EC2 Default User <ec2-user@ip-172-31-26-150.ap-northeast-1.compute.internal>
2024-07-28 04:11:38 +0000

    kurasugatawo
    end

--- chapter9/main.java
+++ chapter9/main.java
@@ -1,12 +1,16 @@
 public class main {
   public static void main(String[] args) {
-    Sword s = new Sword();
-    s.name = "炎の剣";
-    s.damege = 10;
-    Hero h = new Hero();
-    h.name = "湊";
-    h.hp = 100;
-    h. sword = s;
-    System.out.println("現在の武器は" + h.sword.name);
+    Hero h1 = new Hero();
+    h1.name = "湊";
+    h1.hp = 100;
+    Hero h2 = new Hero();
+    h2.name = "朝香";
+    h2.hp = 100;
+    Wizard w = new Wizard();
+    w.name = "菅原";
+    w.hp = 50;
+    w.heal(h1);
+    w.heal(h2);
+    w.heal(h2);
   }
 }
