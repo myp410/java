46d4f8d3a1696cd1d4ece1b75036cdef10e3fdbc
EC2 Default User <ec2-user@ip-172-31-26-150.ap-northeast-1.compute.internal>
2024-07-30 00:34:16 +0000

    親インスタンスへのアクセス

--- chapter10/SuperHero.java
+++ chapter10/SuperHero.java
@@ -11,4 +11,14 @@ public class SuperHero extends Hero {
   public void run() {
     System.out.println(this.name + "は撤退した");
   }
+  public void attack(Matango m) {
+    System.out.println(this.name + "の攻撃");
+    m.hp -= 5;
+    System.out.println("5ポイントのダメージを与えた");
+    if (this.flying) {
+      System.out.println(this.name + "の攻撃");
+      m.hp -= 5;
+      System.out.println("５ポイントのダメージを与えた");
+    }
+  }
 }
