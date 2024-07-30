06f16d701af026a7ddbb3013e21a9b40f9388450
EC2 Default User <ec2-user@ip-172-31-26-150.ap-northeast-1.compute.internal>
2024-07-30 00:16:42 +0000

    継承

--- chapter10/SuperHero.java
+++ chapter10/SuperHero.java
@@ -1,15 +1,5 @@
-public class SuperHero {
-  String name = "湊";
-  int hp = 100;
+public class SuperHero extends Hero {
   boolean flying;
-  public void attack(Matango m) {
-    System.out.println(this.name + "の攻撃！");
-    m.hp -= 5;
-    System.out.println("５ポイントのダメージを与えた！");
-  }
-  public void run() {
-    System.out.println(this.name + "は逃げ出した！");
-  }
   public void fly() {
     this.flying = true;
     System.out.println("飛び上がった");
