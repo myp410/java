a4989e385274ef81df53595cebc46eaf358d2543
EC2 Default User <ec2-user@ip-172-31-26-150.ap-northeast-1.compute.internal>
2024-07-31 22:45:46 +0000

    3つの心配事の解決

--- chapter11/Character.java
+++ chapter11/Character.java
@@ -1,12 +1,9 @@
-public class Character {
+public abstract class Character {
   String name; 
   int hp;
   public void run() {
     System.out.println(this.name + "は逃げ出した");
   }
-  public void attack(Matango m) {
-    System.out.println(this.name + "の攻撃");
-    m.hp -= ;
-    System.out.println("敵にポイントのダメージを与えた");
+  public abstract void attack(Matango m) {
   }
 }
