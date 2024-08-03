f0b190847c8331d379d5bb014ac0f34d576b8f48
EC2 Default User <ec2-user@ip-172-31-26-150.ap-northeast-1.compute.internal>
2024-08-03 01:37:57 +0000

    はこの中身

--- chapter12/Main.java
+++ chapter12/Main.java
@@ -1,9 +1,8 @@
 public class Main {
   public static void main(String[] args) {
-    Wizard w = new Wizard();
-    Matango m = new Matango();
-    w.name = "朝";
-    w.attack(m);
-    w.fireball(m);
+    Slime s = new Slime();
+    Monster m = new Slime();
+    s.run();
+    m.run();
   }
 }
---
+++ chapter12/Slime.java
@@ -0,0 +1,5 @@
+public class Slime extends Monster {
+  public void run() {
+    System.out.println("スライムはささっと逃げ出した");
+  }
+}
