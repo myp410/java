78da94b5772cb2fa3853f43fbd5baa6f5936a504
EC2 Default User <ec2-user@ip-172-31-26-150.ap-northeast-1.compute.internal>
2024-08-03 02:01:53 +0000

    同じ指示で異なる動作

--- chapter12/Main.java
+++ chapter12/Main.java
@@ -9,5 +9,12 @@ public class Main {
     for (Character cj : c) {
       ch.hp += 50;
     }
+    Monster[] monsters = new Monster[3];
+    monsters[0] = new Slime();
+    monsters[1] = new Goblin();
+    monsters[2] = new DeathBat();
+    for (Monster m : monsters) {
+      m.run();
+    }
   }
 }
