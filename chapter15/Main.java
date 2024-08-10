c8f4519adebe148f0c6d889ca0a386cfeee34923
EC2 Default User <ec2-user@ip-172-31-26-150.ap-northeast-1.compute.internal>
2024-08-10 07:08:05 +0000

    連結

--- chapter15/Main.java
+++ chapter15/Main.java
@@ -1,12 +1,9 @@
 public class Main {
   public static void main(String[] args) {
-    String s1 = "Java and JavaScript";
-    if (s1.contains("Java")) {
-      System.out.println("文字列s1は、Javaを含んでいます");
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < 10000; i++) {
+      sb.append("Java");
     }
-    if (s1.endsWith("Java")){
-      System.out.println("文字列s1は、Javaが末尾にあります");
-    }
-    System.out.println("文字列s1で最初にJavaが登場するいちは" + s1.indexOf("Java"));
-    System.out.println("文字列s1で最後にJavaは登場するいちは" + s1.lastIndexOf("Java"));
+    String s = sb.toString();
+    System.out.println(s);
   }
