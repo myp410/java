dc423c0b72b1a7262d32d87484bfecb765867b6e
EC2 Default User <ec2-user@ip-172-31-26-150.ap-northeast-1.compute.internal>
2024-08-10 07:15:05 +0000

    条件設定

--- chapter15/Main.java
+++ chapter15/Main.java
@@ -7,3 +7,20 @@ public class Main {
     String s = sb.toString();
     System.out.println(s);
   }
+  public boolean isValidPlayerName(String name) {
+    if (name.length() != 8) {
+      return false;
+    }
+    char first = name.charAt(0);
+    if(!(first >= 'A' && first <= 'Z')) {
+      return false;
+    }
+    for (int i = 1; i < 8; i++) {
+      char c = name.charAt(i);
+      if (!((c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9'))) {
+        return false;
+      }
+    }
+    return true;
+  }
+}  
