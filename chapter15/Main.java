a6658290253e98feef99e9791e08b21b32993141
EC2 Default User <ec2-user@ip-172-31-26-150.ap-northeast-1.compute.internal>
2024-08-11 22:59:39 +0000

    Calender

--- chapter15/Main.java
+++ chapter15/Main.java
@@ -1,11 +1,15 @@
-import java.util.Date;
+import java.util.*;
 
 public class Main {
   public static void main(String[] args) {
+    Calender c = Calender.getInstance();
+    c.set(2023,8,18,5,53,20);
+    c.set(Calender.MONTH,9);
+    Date d = c.getTime();
+    System.out.println(d);
     Date now = new Date();
-    System.out.println(now);
-    System.out.println(now.getTime());
-    Date past = new Date(16949984000000L);
-    System.out.println(past);
+    c.setTime(now);
+    int y = c.get(Calender.YEAR);
+    System.out.println("今年は" + y + "年です");
   }
 }  
