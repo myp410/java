40fd03f49c376406c952b7bcc9e70608e7a84c06
EC2 Default User <ec2-user@ip-172-31-26-150.ap-northeast-1.compute.internal>
2024-08-18 04:40:14 +0000

    try-with-resource

--- chapter17/main.java
+++ chapter17/main.java
@@ -2,20 +2,10 @@ import java.io.*;
 
 public class Main {
   public static void main(String[] args) {
-    FileWriter fw = null;
-    try {
-      fw = new FileWriter("data.txt");
-      fw.write("hello!");
+    try (FileWriter fw = new FileWriter("data.txt");) {
+      fw.write("hello!"");
     } catch (Exception e) {
       System.out.println("何らかの例外が発生しました");
-    } finally {
-      if (fw != null) {
-        try {
-          fw.close();
-        } catch (IOException e) {
-          ;
-        }
-      }
     }
   }
 }
