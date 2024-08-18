7db5ee843f86e3aba69a20141faf3e0da3d48c7b
EC2 Default User <ec2-user@ip-172-31-26-150.ap-northeast-1.compute.internal>
2024-08-18 04:27:44 +0000

    ざっくり例外を捉える

--- chapter17/main.java
+++ chapter17/main.java
@@ -3,10 +3,11 @@ import java.io.*;
 public class Main {
   public static void main(String[] args) {
     try {
-       FileWriter fw = new FileWriter("data.text");
-    } catch (IOExcaption e) {
-      System.out.println("エラーが発生しました");
-    } 
-   
+      FileWriter fw = new FileWriter("data.txt");
+      fw.write("hello!");
+      fw.close();
+    } catch (Exception e) {
+      System.out.println("何らかの例外が発生しました");
+    }
   }
 }
