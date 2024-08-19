e7a6dd6406c144330654d4c25245589e3ead5767
EC2 Default User <ec2-user@ip-172-31-26-150.ap-northeast-1.compute.internal>
2024-08-19 23:22:30 +0000

    WEBページの取得

--- chapter18/Main.java
+++ chapter18/Main.java
@@ -1,11 +1,14 @@
-import java.io.*;
+import java.io.InputStream;
+import java.net.URL;
 
 public class Main {
   public static void main(String[] args) throws Exception {
-    FileWriter fw = new FileWriter("data.txt");
-    fw.write('そ');
-    fw.write('れ');
-    fw.write('で');
-    fw.write('は');
-    fw.close();
+    URL u = new URL("http");
+    InputStrem is = u.openStream();
+    int i = is.read();
+    while (i != -1) {
+      char c = (char)i;
+      System.out.print(c);
+      i = is.read();
+    }
 }
