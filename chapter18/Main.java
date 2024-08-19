e5825f04696eaab0fcba345fc2e9f02529d31624
EC2 Default User <ec2-user@ip-172-31-26-150.ap-northeast-1.compute.internal>
2024-08-19 23:18:03 +0000

    書き込む

--- chapter18/Main.java
+++ chapter18/Main.java
@@ -2,12 +2,10 @@ import java.io.*;
 
 public class Main {
   public static void main(String[] args) throws Exception {
-    FileReader fr = new FileReader("data.txt");
-    int input = fr.read();
-    while (input != -1) {
-      System.out.println((char)input);
-      input = fr.read();
-    }
-    fr.close();
-  }
+    FileWriter fw = new FileWriter("data.txt");
+    fw.write('そ');
+    fw.write('れ');
+    fw.write('で');
+    fw.write('は');
+    fw.close();
 }
